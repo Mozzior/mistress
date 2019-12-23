b496c9da2d5531e59c11ba50b2f395d7cfb00e85
Mozzior <754012729@qq.com>
2019-12-23 22:55:02 +0800

    init project

---
+++ src/main/java/cn/yushiu/mistress/MistressApplication.java
@@ -0,0 +1,13 @@
+package cn.yushiu.mistress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yushiu/mistress/MistressApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yushiu.mistress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
