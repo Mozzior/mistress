c72c76b25496266a33f09d3c1ad6130253392415
Mozzior <754012729@qq.com>
2019-12-29 23:47:21 +0800

    update thymeleaf config and docker config

--- src/main/java/cn/yushiu/mistress/controller/CoreController.java
+++ src/main/java/cn/yushiu/mistress/controller/CoreController.java
@@ -2,11 +2,12 @@ package cn.yushiu.mistress.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CoreController {
-    @GetMapping("/index")
-    public String index(){
-        return "index";
+    @GetMapping("/{page}")
+    public String page(@PathVariable String page){
+        return page;
     }
 }
